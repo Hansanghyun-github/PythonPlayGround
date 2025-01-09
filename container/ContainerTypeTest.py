9b25617f60f525a80070dad5630c27633f0f04e7
HSH <han41562@gmail.com>
2025-01-09 12:14:22 +0900

    study python - container

---
+++ container/ContainerTypeTest.py
@@ -0,0 +1,15 @@
+def test_list():
+    list1 = [1, 2, 3, 4, 5]
+    assert type(list1) == list
+
+def test_tuple():
+    tuple1 = (1, 2, 3)
+    assert type(tuple1) == tuple
+
+def test_dict():
+    dict1 = {"name": "John", "age": 30}
+    assert type(dict1) == dict
+
+def test_set():
+    set1 = {1, 2, 3, 4, 5}
+    assert type(set1) == set
