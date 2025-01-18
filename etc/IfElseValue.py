7bcd4dee8691f6862f90e0af20bbe07c30004e75
Hansanghyun-github <han41562@gmail.com>
2025-01-18 11:44:19 +0900

    refactor if else value test

--- etc/IfElseValue.py
+++ etc/IfElseValue.py
@@ -1,6 +1,18 @@
+def true_method():
+    return True
+
+def false_method():
+    return False
+
+def value1():
+    return 10
+
+def value2():
+    return 20
+
 def test_if_else_value():
     # if 문이 참이면 앞 값, 거짓이면 뒷 값
-    value = 10 if True else 20
+    value = value1() if true_method() else value2()
     assert value == 10
-    value = 10 if False else 20
+    value = value1() if false_method() else value2()
     assert value == 20
