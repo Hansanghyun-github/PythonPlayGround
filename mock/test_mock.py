87b1877038aca2bf846fbb28dd35d20d975a2134
Hansanghyun-github <han41562@gmail.com>
2025-01-26 15:50:16 +0900

    edit mock test doc

--- mock/test_mock.py
+++ mock/test_mock.py
@@ -1,10 +1,10 @@
 from unittest import mock
 
 class SampleClass:
-    def print_value(self):
-        print('Real Class')
+    def print_value(self) -> str:
+        return 'Real Class'
 
-def test_mocking():
+def test_mocking_method():
     sample = SampleClass()
     sample.print_value = mock.Mock(return_value='Mocked Class')
     assert sample.print_value() == 'Mocked Class'
