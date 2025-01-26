c58cac63a4d03c226108e67c7f686cfb95815bbd
Hansanghyun-github <han41562@gmail.com>
2025-01-26 15:53:48 +0900

    study python - validate mock object is called

--- mock/test_mock.py
+++ mock/test_mock.py
@@ -1,5 +1,7 @@
 from unittest import mock
 
+import pytest
+
 class SampleClass:
     def print_value(self) -> str:
         return 'Real Class'
@@ -8,4 +10,14 @@ def test_mocking_method():
     sample = SampleClass()
     sample.print_value = mock.Mock(return_value='Mocked Class')
     assert sample.print_value() == 'Mocked Class'
+
+def test_mocking_object_call():
+    sample = mock.Mock()
+    sample.print_value()
+    sample.print_value.assert_called_once()
+
+def test_mocking_object_not_call():
+    sample = mock.Mock()
+    with pytest.raises(AssertionError):
+        sample.print_value.assert_called_once()
 
