62dfcc4b26f68d220ef8f9175e7fa9ddfe027aba
Hansanghyun-github <han41562@gmail.com>
2025-01-26 15:57:00 +0900

    study python - mocking static method

--- mock/test_mock.py
+++ mock/test_mock.py
@@ -6,10 +6,25 @@ class SampleClass:
     def print_value(self) -> str:
         return 'Real Class'
 
+    @staticmethod
+    def print_static_value() -> str:
+        return 'Real Static Class'
+
 def test_mocking_method():
     sample = SampleClass()
     sample.print_value = mock.Mock(return_value='Mocked Class')
     assert sample.print_value() == 'Mocked Class'
+
+def test_mocking_static_method():
+    # given
+    SampleClass.print_static_value = mock.Mock(return_value='Mocked Static Class')
+
+    # when
+    return_value = SampleClass.print_static_value()
+
+    # then
+    assert return_value == 'Mocked Static Class'
+    SampleClass.print_static_value.assert_called_once()
 
 def test_mocking_object_call():
     sample = mock.Mock()
