fe2ebdf4ac0cebfc799c0f5543d75feca63c9022
Hansanghyun-github <han41562@gmail.com>
2025-02-02 08:24:42 +0900

    study python - filter method

--- builtins/test_filter.py
+++ builtins/test_filter.py
@@ -1,3 +1,5 @@
+import pytest
+
 def test_filter1():
     # given
     list1 = [1, 2, 3, 4, 5]
@@ -8,3 +10,17 @@ def test_filter1():
 
     # then
     assert result == [2, 4]
+
+def test_filter2():
+    # given
+    list1 = [1, 2, 3, 4, 5]
+
+    # when
+    result = filter(lambda x: x % 2 == 0, list1)
+    # filter({lambda method}, {list})
+
+    # then
+    assert result.__next__() == 2
+    assert result.__next__() == 4
+    with pytest.raises(StopIteration):
+        result.__next__()
