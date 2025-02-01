d407d27de1d8ba2619b8a24ad30ef433aaacf299
Hansanghyun-github <han41562@gmail.com>
2025-02-02 08:09:18 +0900

    add validate list code

--- container/test_list.py
+++ container/test_list.py
@@ -1,16 +1,28 @@
 import pytest
 
 def test_list_size():
+    # when
     ll: list[int] = [1, 2, 3, 4, 5]
+
+    # then
     assert len(ll) == 5
+    assert ((not ll) == False)
 
 def test_list_size_is_zero():
+    # when
     ll: list[int] = []
+
+    # then
     assert len(ll) == 0
     assert ll is not None
+    assert ((not ll) == True)
 
 def test_list_is_none():
+    # when
     ll: list[int] = None
+
+    # then
     assert ll is None
+    assert ((not ll) == True)
     with pytest.raises(TypeError):
         assert len(ll) == 0
