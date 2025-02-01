140c9449e0c774c6d9be616fa51bedb5bdd26482
Hansanghyun-github <han41562@gmail.com>
2025-02-01 13:47:42 +0900

    study python - filter method

---
+++ builtins/test_filter.py
@@ -0,0 +1,10 @@
+def test_filter1():
+    # given
+    list1 = [1, 2, 3, 4, 5]
+
+    # when
+    result = list(filter(lambda x: x % 2 == 0, list1))
+    # filter({lambda method}, {list})
+
+    # then
+    assert result == [2, 4]
