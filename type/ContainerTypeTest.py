8e893ee5433ed5cf12f07c310620745c64fef820
Hansanghyun-github <han41562@gmail.com>
2025-03-02 12:22:56 +0900

    study python - set

--- type/ContainerTypeTest.py
+++ type/ContainerTypeTest.py
@@ -6,10 +6,10 @@ def test_tuple():
     tuple1 = (1, 2, 3)
     assert type(tuple1) == tuple
 
-def test_dict():
-    dict1 = {"name": "John", "age": 30}
-    assert type(dict1) == dict
-
 def test_set():
     set1 = {1, 2, 3, 4, 5}
     assert type(set1) == set
+
+def test_dict():
+    dict1 = {"name": "John", "age": 30}
+    assert type(dict1) == dict
