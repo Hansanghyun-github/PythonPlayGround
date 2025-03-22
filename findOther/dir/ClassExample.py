aaaeaccdeb4184d5f77ae6f9e123abb74332e22c
Hansanghyun-github <han41562@gmail.com>
2025-03-22 21:20:11 +0900

    study import dir and class

---
+++ findOther/dir/ClassExample.py
@@ -0,0 +1,9 @@
+def hello_world() -> str:
+    return "Hello, World!"
+
+class Example:
+    def __init__(self, name: str):
+        self.name = name
+
+    def say_hello(self) -> str:
+        return f"Hello, {self.name}!"
