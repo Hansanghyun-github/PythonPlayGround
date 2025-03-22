8783a878cb86d3f32de35b7afb54cdfcc7e3bd7f
Hansanghyun-github <han41562@gmail.com>
2025-03-22 21:20:52 +0900

    study test nested class

---
+++ nested/NestedClass.py
@@ -0,0 +1,12 @@
+
+class TestCase:
+    def test_case(self):
+        print('test_case')
+
+    class TestNestedCase1:
+        def test_nested_case(self):
+            print('test_nested_case')
+
+    class TestNestedCase2:
+        def test_nested_case(self):
+            print('test_nested_case')
