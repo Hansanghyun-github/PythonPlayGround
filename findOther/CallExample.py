d7bb871e9760cf7859ace1afa60f0ac151cacd52
Hansanghyun-github <han41562@gmail.com>
2025-03-29 20:03:23 +0900

    refactor: separate import code to each test method

--- findOther/CallExample.py
+++ findOther/CallExample.py
@@ -1,14 +1,13 @@
-from dir import ModuleExample
-
 def test_call_method_from_modele():
+    from dir import ModuleExample
     assert ModuleExample.hello_world() == "Hello, World!"
 
 def test_call_class_from_module():
+    from dir import ModuleExample
     example = ModuleExample.Example("World")
     assert example.say_hello() == "Hello, World!"
 
-from dir.ClassExample import Example
-
 def test_call_class_from_class():
+    from dir.ClassExample import Example
     example = Example("World")
     assert example.say_hello() == "Hello, World!"
