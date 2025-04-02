95eba3fae55db2578cc3503097eb5c24a4731e4e
Hansanghyun-github <han41562@gmail.com>
2025-04-02 21:21:14 +0900

    study dict key not found case

---
+++ container/test_dict.py
@@ -0,0 +1,30 @@
+import pytest
+from collections import defaultdict
+
+def test_dict_not_found():
+    # given
+    dict: dict[str, int] = {}
+
+    # when # then
+    with pytest.raises(KeyError):
+        dict['key']
+
+def test_dict_defaultdict_not_found():
+    # given
+    dict: defaultdict[str, int] = defaultdict(int)
+
+    # when
+    value = dict['key']
+
+    # then
+    assert value == 0
+
+def test_dict_defaultdict_not_found_str():
+    # given
+    dict: defaultdict[str, str] = defaultdict(str)
+
+    # when
+    value = dict['key']
+
+    # then
+    assert value == ''
