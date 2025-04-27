582fd2e816274ca81af12f7fc5506b8312a19df2
Hansanghyun-github <han41562@gmail.com>
2025-04-27 21:42:00 +0900

    study arrow - span_range

--- datetime/test_datetime.py
+++ datetime/test_datetime.py
@@ -12,3 +12,27 @@ def test_now_timezone2():
     dt2 = arrow.now(tz='UTC').replace(microsecond=0).datetime
 
     assert dt1.time() == (dt2 + timedelta(hours=9)).time()
+
+def test_span_range():
+    datetime = arrow.get('2025-01-01T00:00:00')
+
+    span = arrow.Arrow.span_range('day', datetime, datetime)
+
+    result = list(span)
+    assert len(result) == 1
+    assert result[0][0] == datetime
+    assert result[0][1] == datetime + timedelta(days=1, microseconds=-1)
+
+def test_span_range2():
+    datetime = arrow.get('2025-01-01T00:00:00')
+
+    span = arrow.Arrow.span_range('day', datetime, datetime + timedelta(days=1))
+
+    result = list(span)
+    assert len(result) == 2
+
+    assert result[0][0] == datetime
+    assert result[0][1] == datetime + timedelta(days=1, microseconds=-1)
+
+    assert result[1][0] == datetime + timedelta(days=1)
+    assert result[1][1] == datetime + timedelta(days=2, microseconds=-1)
