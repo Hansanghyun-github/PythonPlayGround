ca97b1646a4943c6954e26d696a85fcc62d5a9b1
Hansanghyun-github <han41562@gmail.com>
2025-04-30 18:17:52 +0900

    add with block test

--- etc/WithTest.py
+++ etc/WithTest.py
@@ -7,9 +7,20 @@ class WithClass:
         self.value += 3
 
 
-def test_with():
+def test_with_block():
     target = WithClass()
     with target:
         print('inside with block')
 
     assert target.value == 6
+
+def test_with_block_with_error():
+    target = WithClass()
+    try:
+        with target:
+            raise Exception('test exception')
+    except Exception as e:
+        print('exception:', e)
+
+    assert target.value == 6
+    # if error is occurred, __exit__ method will be called
