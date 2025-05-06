d475adfbc0d49eae9092a6b063b3f12bd351d6db
Hansanghyun-github <han41562@gmail.com>
2025-05-06 17:07:43 +0900

    add async test with sleep

--- async/test_async.py
+++ async/test_async.py
@@ -42,3 +42,22 @@ def test_execute_async_method_with_asyncio_gather():
 
     # then
     assert 3 <= end - start < 4
+
+async def task_with_sleep(second: int):
+    print(f"Task {second} started")
+    time.sleep(second)
+    print(f"Task {second} finished")
+
+def test_execute_async_method_with_sleep():
+    # given
+    async def run_all():
+        await asyncio.gather(task_with_sleep(1), task_with_sleep(2), task_with_sleep(3))
+
+    # when
+    start = time.time()
+    asyncio.run(run_all())
+    end = time.time()
+
+    # then
+    assert end - start >= 6
+    # sleep is blocking the thread, so it will not be executed in parallel
